7f7be3f4502fb9eeede7e282a4eed9dd494cdf59
rarosario <rafael.rosario@mercadolivre.com>
2021-08-20 13:45:03 -0300

    aula Java 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // Exercicio 1
+//        String apelido = "Gomez";
+//        int idade = 35;
+//        boolean b = false;
+//        double saldo = 45857.90;
+//        String nome = "Julián";
+
+        // Exercicio 2
+//        String 1nome;   // Deve começar com letra
+//        int idade;
+//        double $aldo;   // Sem caractere especial
+//        String direcao;
+//        boolean carteira de motorista; // Sem espaços
+//        double altura de pessoa;    // não usar letras
+//        int quantidade-de-filhos;   //numeros
+
+        // Exercicio 3
+//        int valor = 2;
+//        int dias = 5;
+//        System.out.println(valor * dias);
+
+        // Exercicio 4
+//        Scanner entrada  = new Scanner(System.in);
+//        System.out.println(“Entre o valor do prêmio!“);
+//        double valorPremio = entrada.nextDouble();
+//        double valorImpostos = (2.45d+15d+3d)/100d;
+//        double total = valorPremio*valorImpostos;
+//        DecimalFormat df = new DecimalFormat(“.##“);
+//        df.setRoundingMode(RoundingMode.DOWN);
+//        System.out.println(df.format(total));
+    }
+}
